10cf4c8524cee866f74de76508b20f2a1482701b
Alexey I. Churkin <phoebus@osiacat.ru>
2009-11-03 15:44:36 +0200

    + forgotten class DoxyGroupBuilder

--- lib/DoxyGen/DoxyGroupBuilder.class.php
+++ lib/DoxyGen/DoxyGroupBuilder.class.php
@@ -7,15 +7,37 @@
  *
  * Copyright (c) 2009 phoebius.org
  *
- * This program is free software; you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Foundation;
- * either version 3 of the License, or (at your option) any later version.
- *
- * You should have received a copy of the GNU Lesser General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses/>.
+ * All rights reserved.
  *
  ************************************************************************************************/
 
+/**
+ * @ingorup DoxyGen
+ */
+abstract class DoxyGroupBuilder
+{
+	/**
+	 * @var DoxyGroup
+	 */
+	private $rootDoxyGroup;
 
+	/**
+	 * @return DoxyGroup
+	 */
+	abstract function build();
+
+	function __construct(DoxyGroup $rootDoxyGroup = null)
+	{
+		$this->rootDoxyGroup =
+			$rootDoxyGroup
+				? $rootDoxyGroup
+				: new DoxyGroup('');
+	}
+
+	function getRootDoxyGroup()
+	{
+		return $this->rootDoxyGroup;
+	}
+}
 
 ?>
