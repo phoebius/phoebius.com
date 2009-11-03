d5f50cee2d095a7619816d4abd34575b1d9d9ce7
Alexey I. Churkin <phoebus@osiacat.ru>
2009-11-03 10:04:59 +0200

    * css refactoring (google search moved to view)

--- views/search.view.php
+++ views/search.view.php
@@ -45,22 +45,27 @@ $this->setMaster(
             <div class="container">
               <h1>Search through phoebius</h1>
 
+<style type="text/css">
+#content .gsc-control {
+	width:100%;
+}
+</style>
               <div class="article" id="content">Searching...</div>
 
-			<script language="Javascript" type="text/javascript">
-				var query = '<?=addslashes($query)?>';
-				var searchControl = new GSearchControl();
-				var webSearch = new GwebSearch();
-				webSearch.setSiteRestriction ("phoebius.org")
-				options = new GsearcherOptions();
-				options.setExpandMode (GSearchControl.EXPAND_MODE_OPEN);
-				searchControl.addSearcher (webSearch, options);
-				searchControl.setResultSetSize (GSearch.LARGE_RESULTSET);
-				searchControl.setLinkTarget (GSearch.LINK_TARGET_SELF);
-				searchControl.draw (document.getElementById ("content"));
-				if (query)
-					searchControl.execute(query);
-			</script>
+<script language="Javascript" type="text/javascript">
+	var query = '<?=addslashes($query)?>';
+	var searchControl = new GSearchControl();
+	var webSearch = new GwebSearch();
+	webSearch.setSiteRestriction ("phoebius.org")
+	options = new GsearcherOptions();
+	options.setExpandMode (GSearchControl.EXPAND_MODE_OPEN);
+	searchControl.addSearcher (webSearch, options);
+	searchControl.setResultSetSize (GSearch.LARGE_RESULTSET);
+	searchControl.setLinkTarget (GSearch.LINK_TARGET_SELF);
+	searchControl.draw (document.getElementById ("content"));
+	if (query)
+		searchControl.execute(query);
+</script>
          </div>
          <!--Main content-->
       </div>
