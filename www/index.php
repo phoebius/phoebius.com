4c99cb8e7196999c6f1c377be7dbb204242b73d4
Alexey I. Churkin <phoebus@osiacat.ru>
2009-11-16 17:01:39 +0200

    * application initializer code moved to a separate class-based wrapper

--- www/index.php
+++ www/index.php
@@ -24,61 +24,13 @@ require ( APP_ROOT . '/etc/config.php' );
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-try
-{
-	require
+require
 		APP_ROOT . DIRECTORY_SEPARATOR .
 		'cfg' . DIRECTORY_SEPARATOR .
 		APP_SLOT . DIRECTORY_SEPARATOR .
 		'config.php';
 
-	//
-	// your stuff goes here
-	//
-
-	$request = new WebRequest(
-		new WebRequestDictionary($_SERVER),
-		$_GET, $_POST, $_COOKIE, $_FILES
-	);
-
-	$webContext = new WebContext(
-		$request,
-		new WebResponse(true, $request),
-		new WebServerState(new WebServerStateDictionary($_SERVER))
-	);
-
-	$router = new PhoebiusRouter();
-
-	$trace = $router->getTrace($webContext);
+$application = new StandaloneSiteApplication();
+$application->run();
 	
-	$trace->handle();
-}
-catch (Exception $e)
-{
-	if (APP_SLOT_CONFIGURATION & SLOT_CONFIGURATION_FLAG_DEVELOPMENT) {
-		//echo '<pre>', $e->getTraceAsString();
-		throw $e;
-	}
-	else {
-		mail(
-			ConfigurationEntry::getEntry(
-				new ConfigurationKey(ConfigurationKey::ADMIN_EMAIL)
-			)->getValue(),
-			"[Phoebius crash] phoebius.org crash #" . substr(sha1($e->getMessage()), 0, 6),
-			join(
-				"\n\n",
-				array(
-					$_SERVER['REQUEST_URI'],
-					'Request method: ' . $_SERVER['REQUEST_METHOD'],
-					get_class($e). ' msg: ' . $e->getMessage(),
-					print_r((array)$e, true),
-					$e->getTraceAsString()
-				)
-			)
-		);
-	}
-
-	$router->getFallbackTrace()->handle($trace);
-}
-
 ?>
